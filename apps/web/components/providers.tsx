ee83fe5166b685c4f9e849f7fd0c1c28a204c4f7
Matt Welson <matthew.welson@gmail.com>
2025-08-06 15:11:28 +1200

    feat(auth): adding clerk and lining to Convex (#2)
    * feat(auth): adding clerk and lining to Convex
    
    * Update packages/backend/convex/auth.config.ts
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * fix(db): adding security check to getMany query
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -1,23 +1,25 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { ConvexProvider, ConvexReactClient } from "convex/react"
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { useAuth } from "@clerk/nextjs";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL ?? '');
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL ?? "");
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ConvexProvider client={convex}>
-    <NextThemesProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-      enableColorScheme
-    >
-      {children}
-    </NextThemesProvider>
-    </ConvexProvider>
-  )
+    <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+      <NextThemesProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+        enableColorScheme
+      >
+        {children}
+      </NextThemesProvider>
+    </ConvexProviderWithClerk>
+  );
 }
