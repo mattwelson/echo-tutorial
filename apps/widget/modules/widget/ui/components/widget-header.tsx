dd49b2891fe30911fd934164dddf6d59b4e2a741
Matt Welson <matt@digitaltransformation.co.nz>
2025-08-12 13:41:37 +1200

    feat(widget/ui): :art: layout on widget
    Stupid gradient didn't work on Safari - similar issue to the web active button
    
    Create issues

---
+++ apps/widget/modules/widget/ui/components/widget-header.tsx
@@ -0,0 +1,17 @@
+import { cn } from "@workspace/ui/lib/utils";
+import { ReactNode } from "react";
+
+export function WidgetHeader({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
+  return (
+    // TODO: gradient is not working on safari and is ticking me off
+    <header className={cn("bg-primary p-4 text-primary-foreground", className)}>
+      {children}
+    </header>
+  );
+}
